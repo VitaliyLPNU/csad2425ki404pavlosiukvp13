95aef7f1806b1d922460f791c48f454b03401656
VitaliyLPNU <vitalii.pavlosiuk.ki.2021@lpnu.ua>
2024-10-27 21:27:41 +0200

    updated

--- sw_client/cmake/windeployqt.cmake
+++ sw_client/cmake/windeployqt.cmake
@@ -1,29 +1,11 @@
-find_package(Qt5Core REQUIRED)
+# windeployqt.cmake
 
-# get absolute path to qmake, then use it to find windeployqt executable
-
-get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
-get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-
-function(windeployqt target)
-
-    # POST_BUILD step
-    # - after build, we have a bin/lib for analyzing qt dependencies
-    # - we run windeployqt on target and deploy Qt libs
-
-    add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND "${_qt_bin_dir}/windeployqt.exe"         
-                --verbose 1
-		#--release
-                --no-svg
-                --no-angle
-                --no-opengl
-                --no-opengl-sw
-                --no-compiler-runtime
-                --no-system-d3d-compiler
-		--no-translations
-                \"$<TARGET_FILE:${target}>\"
-        COMMENT "Deploying Qt libraries using windeployqt for compilation target '${target}' ..."
-    )
-
+# Функція для виконання windeployqt для Qt-додатка
+function(deploy_qt_app target)
+    if(WIN32 AND Qt6_FOUND)
+        add_custom_command(TARGET ${target} POST_BUILD
+            COMMAND ${Qt6_DIR}/../../../bin/windeployqt.exe $<TARGET_FILE:${target}>
+            COMMENT "Running windeployqt for ${target}"
+        )
+    endif()
 endfunction()
